d1525aea9e59bd8d26a4f6f886af71e1ef5e321e
Jonathan Warner <jewarner57@gmail.com>
2021-01-11 16:10:46 -0500

    feat: finish font weight slider

--- static/view-settings.js
+++ static/view-settings.js
@@ -1,11 +1,34 @@
 $(function () {
-    // change upload icon to check on upload
-    $("#image-upload").change(function () {
-        $("#upload-icon").html("check")
+    // change font weight
+    $("#font-weight").change(function () {
+
+        fontWeight = $("#font-weight").val()
+
+        $(".ascii-display").css({ "font-weight": fontWeight })
     })
 
-    // display loading bar when submit button is pressed
-    $("#submit-image").click(function () {
-        $("#conversion-progress").removeClass("hidden")
+    $("font-family").change(function () {
+        fontFamilyNum = $("font-family").val()
+        font = ""
+
+        switch (fontFamilyNum) {
+            case 1:
+                font = "'Source Code Pro', monospace;"
+                break;
+
+            case 2:
+                font = "'Courier New', Courier, monospace"
+                break;
+
+            case 3:
+                font = "'Courier New', Courier, monospace"
+                break;
+
+            default:
+                font = "'Courier New', Courier, monospace"
+
+        }
+
+        $(".ascii-display").css({ "font-family": font })
     })
 })
