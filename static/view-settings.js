d63ca00937a2b2ed234989f6886889d5383c7c45
Jonathan Warner <jewarner57@gmail.com>
2021-01-11 16:28:08 -0500

    feat: add font selector functionality

--- static/view-settings.js
+++ static/view-settings.js
@@ -7,21 +7,27 @@ $(function () {
         $(".ascii-display").css({ "font-weight": fontWeight })
     })
 
-    $("font-family").change(function () {
-        fontFamilyNum = $("font-family").val()
+    $("#font-family").change(function () {
+        fontFamilyNum = $("#font-family").val()
         font = ""
 
+        console.log(fontFamilyNum)
+
         switch (fontFamilyNum) {
-            case 1:
-                font = "'Source Code Pro', monospace;"
+            case "1":
+                font = "'Source Code Pro', monospace"
                 break;
 
-            case 2:
+            case "2":
                 font = "'Courier New', Courier, monospace"
                 break;
 
-            case 3:
-                font = "'Courier New', Courier, monospace"
+            case "3":
+                font = "'DM Mono', monospace"
+                break;
+
+            case "4":
+                font = "'Space Mono', monospace"
                 break;
 
             default:
